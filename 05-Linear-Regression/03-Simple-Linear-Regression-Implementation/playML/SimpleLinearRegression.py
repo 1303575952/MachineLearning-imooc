be0c4dfb4b99055e2508342d65bf8ff9989c5731
1303575952 <1303575952@qq.com>
2018-10-28 22:29:52 +0800

    SimpleLinearRegression

--- 05-Linear-Regression/03-Simple-Linear-Regression-Implementation/playML/SimpleLinearRegression.py
+++ 05-Linear-Regression/03-Simple-Linear-Regression-Implementation/playML/SimpleLinearRegression.py
@@ -10,10 +10,8 @@ class SimpleLinearRegression1:
 
     def fit(self, x_train, y_train):
         """根据训练数据集x_train,y_train训练Simple Linear Regression模型"""
-        assert x_train.ndim == 1, \
-            "Simple Linear Regressor can only solve single feature training data."
-        assert len(x_train) == len(y_train), \
-            "the size of x_train must be equal to the size of y_train"
+        assert x_train.ndim == 1, "Simple Linear Regressor can only solve single feature training data."
+        assert len(x_train) == len(y_train), "the size of x_train must be equal to the size of y_train"
 
         x_mean = np.mean(x_train)
         y_mean = np.mean(y_train)
@@ -31,10 +29,8 @@ class SimpleLinearRegression1:
 
     def predict(self, x_predict):
         """给定待预测数据集x_predict，返回表示x_predict的结果向量"""
-        assert x_predict.ndim == 1, \
-            "Simple Linear Regressor can only solve single feature training data."
-        assert self.a_ is not None and self.b_ is not None, \
-            "must fit before predict!"
+        assert x_predict.ndim == 1, "Simple Linear Regressor can only solve single feature training data."
+        assert self.a_ is not None and self.b_ is not None, "must fit before predict!"
 
         return np.array([self._predict(x) for x in x_predict])
 
